a5843c51dd0b84aac8846153db592580f731e246
hyym3349 <44021692+hyym3349@users.noreply.github.com>
2021-06-25 15:34:28 +0900

    board_1

--- TestPjt/src/main/java/com/project/dao/BoardDAOImpl.java
+++ TestPjt/src/main/java/com/project/dao/BoardDAOImpl.java
@@ -2,6 +2,8 @@ package com.project.dao;
 
 import javax.inject.Inject;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +20,32 @@ public class BoardDAOImpl implements BoardDAO {
 	public void write(BoardVO boardVO) throws Exception {
 		sqlSession.insert("boardMapper.insert", boardVO);
 		
+	}
+	// 게시물 목록 조회
+	@Override
+	public List<BoardVO> list() throws Exception {
+	
+		return sqlSession.selectList("boardMapper.list");
+
+	}
+	// 게시물 조회
+	@Override
+	public BoardVO read(int bno) throws Exception {
+			
+		return sqlSession.selectOne("boardMapper.read", bno);
+	}
+	// 게시물 수정
+	@Override
+	public void update(BoardVO boardVO) throws Exception {
+		
+		sqlSession.update("boardMapper.update", boardVO);
+	}
+
+	// 게시물 삭제
+	@Override
+	public void delete(int bno) throws Exception {
+		
+		sqlSession.delete("boardMapper.delete", bno);
 	}
 
 }
--- TestPjt/src/main/java/com/project/service/BoardServiceImpl.java
+++ TestPjt/src/main/java/com/project/service/BoardServiceImpl.java
@@ -3,6 +3,8 @@ package com.project.service;
 
 import javax.inject.Inject;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.project.dao.BoardDAO;
@@ -18,6 +20,34 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	public void write(BoardVO boardVO) throws Exception {
 		dao.write(boardVO);
+	}
+	
+	// 게시물 목록 조회
+	@Override
+	public List<BoardVO> list() throws Exception {
+
+		return dao.list();
+	}
+	
+	// 게시물 목록 조회
+	@Override
+	public BoardVO read(int bno) throws Exception {
+
+		return dao.read(bno);
+	}
+	
+	// 게시물 목록 수정
+	@Override
+	public void update(BoardVO boardVO) throws Exception {
+
+		dao.update(boardVO);
+	}
+
+	// 게시물 목록 삭제
+	@Override
+	public void delete(int bno) throws Exception {
+		
+		dao.delete(bno);
 	}
 
 }
